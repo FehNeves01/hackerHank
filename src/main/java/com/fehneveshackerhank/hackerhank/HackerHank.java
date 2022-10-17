dfc06e05a3d897075f406e2cdf1c93d32c4ce0f5
Felipe <felipe.neves01@gmail.com>
2022-10-17 17:13:25 -0300

    adicionando desafio

--- src/main/java/com/fehneveshackerhank/hackerhank/HackerHank.java
+++ src/main/java/com/fehneveshackerhank/hackerhank/HackerHank.java
@@ -10,6 +10,10 @@ import java.util.List;
 /**
  *
  * @author felipeneves
+ *
+ * link desafio
+ *
+ * https://www.hackerrank.com/challenges/sock-merchant/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
  */
 public class HackerHank {
 
